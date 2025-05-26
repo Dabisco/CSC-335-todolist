734ebce328cf54128bb8c0f570491f8578897a3b
abiola9525 <omotosoabiola9525@gmail.com>
2025-05-26 11:57:38 +0100

    railway push

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . models import TodoStatus, TodoPriority, Todo
 # Register your models here.
+
+admin.site.register(TodoStatus)
+admin.site.register(TodoPriority)
+admin.site.register(Todo)
